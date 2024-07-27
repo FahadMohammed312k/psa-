b9f04e8f2f868e1c129f8a468f0ee49951066a86
user <mohammed312k@gmail.com>
2024-07-27 15:31:22 +0530

    Defect 3457 fixed the link that were not working in homw page

---
+++ src/test/java/come/git_example/GitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package come.git_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
